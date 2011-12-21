c3c427dfb979aca234afb0697e9979b614bd2be6
desanlee <desanlee@gmail.com>
2011-12-21 17:03:17 +0800

    All begins

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leju::Application.config.secret_token = '72c16a7d6579605f7bfa231493be7e85b69e80018e1f8e895a80572f3eca9ffaeecfa0354f5ee8bee96c7ecb7386795bcc697d56e3dfe571d692b42cb995b641'
