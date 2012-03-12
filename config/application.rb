af23fd39948cc37eaab84a684ad9a3b0069d769b
desanlee <desanlee@gmail.com>
2012-03-12 22:04:42 +0800

    change readme

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module Leju
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+	config.active_record.default_timezone = :local
+    config.time_zone = 'Beijing'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
